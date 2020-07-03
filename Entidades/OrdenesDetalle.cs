3133a92f8c5032a799b8609262bce1e35b3c19fd
Victor Johan <Vicjohan23@gmail.com>
2020-07-02 22:08:23 -0400

    SE CREARON TODAS LAS ENTIDADES Y SE AGREGARON LOS RECURSOS A UTILIZAR.

---
+++ Entidades/OrdenesDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OrdenSuplidor.Entidades
+{
+    public class OrdenesDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int OrdenId { get; set; }
+        public int Cantidad { get; set; }
+        public double Costo { get; set; }
+
+        public OrdenesDetalle(int id, int ordenId, int cantidad, double costo)
+        {
+            Id = id;
+            OrdenId = ordenId;
+            Cantidad = cantidad;
+            Costo = costo;
+        }
+
+    }
+}
---
+++ Entidades/Suplidores.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OrdenSuplidor.Entidades
+{
+    public class Suplidores
+    {
+        [Key]
+        public int SuplidorId { get; set; }
+        public string Nombres { get; set; }
+    }
+}
